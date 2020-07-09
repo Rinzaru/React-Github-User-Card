d212cc7bed9a41d882231939256924952ada7649
unknown <rinzaru@gmail.com>
2020-07-08 23:34:17 -0600

    added usergraph

--- src/components/users.js
+++ src/components/users.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Card } from "reactstrap";
+import GitHubCalendar from "react-github-calendar";
 class User extends React.Component {
   render(props) {
     console.log(this.props.data);
+
     // console.log(mapData, "follower data");
     return (
       <div>
         {this.props.data.map((data) => {
           return (
-            <Card style={{ margin: "auto", width: "28%", marginTop: "10px" }}>
+            <Card style={{ margin: "auto", width: "50%", marginTop: "10px" }}>
               <img
                 src={data.avatar_url}
                 alt="No img"
@@ -18,6 +20,7 @@ class User extends React.Component {
               <p style={{ margin: "auto" }}>Bio: {data.bio}</p>
               <p style={{ margin: "auto" }}>followers: {data.followers}</p>
               <p style={{ margin: "auto" }}>Following: {data.following}</p>
+              <GitHubCalendar username={data.login} />
             </Card>
           );
         })}
